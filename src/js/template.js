2745ddfb9433e70638ad247f9005f5a52ad77427
Adam Kocić <adam.kocic@automatkredytowy.pl>
2018-06-13 16:54:12 +0200

    init

---
+++ src/js/template.js
@@ -0,0 +1,137 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['buoy'], factory(root));
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('buoy'));
+    } else {
+        root.myPlugin = factory(root, root.buoy);
+    }
+})(typeof global !== 'undefined' ? global : this.window || this.global, function (root) {
+
+    'use strict';
+
+    //
+    // Variables
+    //
+
+    const myPlugin = {}; // Object for public APIs
+    const supports = !!document.querySelector && !!root.addEventListener; // Feature test
+    let settings; // Placeholder variables
+
+    // Default settings
+    const defaults = {
+        resizeLog: 'The window was resized!',
+        callbackBefore: function () {
+        },
+        callbackAfter: function () {
+        }
+    };
+
+
+    //
+    // Methods
+    //
+
+    /**
+     * Add a class to a link when it's clicked
+     * @private
+     * @param {Event} event The click event
+     */
+    const addClass = function (event) {
+
+        // Get the thing that was clicked
+        const toggle = event.target;
+
+        // Check if the thing that was clicked has the [data-click-me] attribute
+        if (toggle && toggle.hasAttribute('data-click-me')) {
+
+            // Prevent default click event
+            if (toggle.tagName.toLowerCase() === 'a') {
+                event.preventDefault();
+            }
+
+            // Set the [data-click-me] value as a class on the link
+            toggle.classList.add(toggle.getAttribute('data-click-me'));
+
+        }
+
+    };
+
+    /**
+     * Handle events
+     * @private
+     */
+    const eventHandler = function (event) {
+
+        // Callback before the event handler runs
+        settings.callbackBefore(myPlugin);
+
+        // On click
+        if (event.type === 'click') {
+            addClass(event);
+        }
+
+        // On resize
+        if (event.type === 'resize') {
+            console.log(settings.resizeLog);
+        }
+
+        // Callback after the event handler runs
+        settings.callbackAfter(myPlugin);
+
+    };
+
+    /**
+     * Destroy the current initialization.
+     * @public
+     */
+    myPlugin.destroy = function () {
+
+        // If plugin isn't already initialized, stop
+        if (!settings) return;
+
+        // Remove all added classes
+        const links = document.querySelectorAll('[data-click-me]');
+        for (let i = 0, len = links.length; i < len; i++) {
+            links[i].classList.remove(links[i].getAttribute('data-click-me'));
+        }
+
+        // Remove event listeners
+        document.removeEventListener('click', eventHandler, false);
+        window.removeEventListener('resize', eventHandler, false);
+
+        // Reset variables
+        settings = null;
+
+    };
+
+    /**
+     * Initialize Plugin
+     * @public
+     * @param {Object} options User settings
+     */
+    myPlugin.init = function (options) {
+
+        // feature test
+        if (!supports) return;
+
+        // Destroy any existing initializations
+        myPlugin.destroy();
+
+        // Merge user options with defaults
+        settings = buoy.extend(defaults, options || {});
+
+        // Listen for click events
+        document.addEventListener('click', eventHandler, false);
+        window.addEventListener('resize', eventHandler, false);
+
+    };
+
+
+    //
+    // Public APIs
+    //
+
+    return myPlugin;
+
+});
---
+++ src/js/template3.js
@@ -0,0 +1,206 @@
+/**
+ *
+ * Name v0.0.1
+ * Description, by Chris Ferdinandi.
+ * http://gomakethings.com
+ *
+ * Free to use under the MIT License.
+ * http://gomakethings.com/mit/
+ *
+ */
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory(root));
+    } else if (typeof exports === 'object') {
+        module.exports = factory(root);
+    } else {
+        root.myPlugin = factory(root);
+    }
+})(typeof global !== "undefined" ? global : this.window || this.global, function (root) {
+
+    'use strict';
+
+    //
+    // Variables
+    //
+
+    const myPlugin = {}; // Object for public APIs
+    const supports = !!document.querySelector && !!root.addEventListener; // Feature test
+    let settings, eventTimeout;
+
+    // Default settings
+    const defaults = {
+        someVar: 123,
+        initClass: 'js-myplugin',
+        callbackBefore: function () {
+        },
+        callbackAfter: function () {
+        }
+    };
+
+
+    //
+    // Methods
+    //
+
+    /**
+     * A simple forEach() implementation for Arrays, Objects and NodeLists
+     * @private
+     * @param {Array|Object|NodeList} collection Collection of items to iterate
+     * @param {Function} callback Callback function for each iteration
+     * @param {Array|Object|NodeList} scope Object/NodeList/Array that forEach is iterating over (aka `this`)
+     */
+    const forEach = function (collection, callback, scope) {
+        if (Object.prototype.toString.call(collection) === '[object Object]') {
+            for (let prop in collection) {
+                if (Object.prototype.hasOwnProperty.call(collection, prop)) {
+                    callback.call(scope, collection[prop], prop, collection);
+                }
+            }
+        } else {
+            for (let i = 0, len = collection.length; i < len; i++) {
+                callback.call(scope, collection[i], i, collection);
+            }
+        }
+    };
+
+    /**
+     * Merge defaults with user options
+     * @private
+     * @param {Object} defaults Default settings
+     * @param {Object} options User options
+     * @returns {Object} Merged values of defaults and options
+     */
+    const extend = function (defaults, options) {
+        var extended = {};
+        forEach(defaults, function (value, prop) {
+            extended[prop] = defaults[prop];
+        });
+        forEach(options, function (value, prop) {
+            extended[prop] = options[prop];
+        });
+        return extended;
+    };
+
+    /**
+     * Convert data-options attribute into an object of key/value pairs
+     * @private
+     * @param {String} options Link-specific options as a data attribute string
+     * @returns {Object}
+     */
+    const getDataOptions = function (options) {
+        return !options || !(typeof JSON === 'object' && typeof JSON.parse === 'function') ? {} : JSON.parse(options);
+    };
+
+    /**
+     * Get the closest matching element up the DOM tree
+     * @param {Element} elem Starting element
+     * @param {String} selector Selector to match against (class, ID, or data attribute)
+     * @return {Boolean|Element} Returns false if not match found
+     */
+    const getClosest = function (elem, selector) {
+        const firstChar = selector.charAt(0);
+        for (; elem && elem !== document; elem = elem.parentNode) {
+            if (firstChar === '.') {
+                if (elem.classList.contains(selector.substr(1))) {
+                    return elem;
+                }
+            } else if (firstChar === '#') {
+                if (elem.id === selector.substr(1)) {
+                    return elem;
+                }
+            } else if (firstChar === '[') {
+                if (elem.hasAttribute(selector.substr(1, selector.length - 2))) {
+                    return elem;
+                }
+            }
+        }
+        return false;
+    };
+
+    // @todo Do something...
+
+    /**
+     * Handle events
+     * @private
+     */
+    const eventHandler = function (event) {
+        const toggle = event.target;
+        const closest = getClosest(toggle, '[data-some-selector]');
+        if (closest) {
+            // run methods
+        }
+    };
+
+    /**
+     * Destroy the current initialization.
+     * @public
+     */
+    myPlugin.destroy = function () {
+
+        // If plugin isn't already initialized, stop
+        if (!settings) return;
+
+        // Remove init class for conditional CSS
+        document.documentElement.classList.remove(settings.initClass);
+
+        // @todo Undo any other init functions...
+
+        // Remove event listeners
+        document.removeEventListener('click', eventHandler, false);
+
+        // Reset variables
+        settings = null;
+        eventTimeout = null;
+    };
+
+    /**
+     * On window scroll and resize, only run events at a rate of 15fps for better performance
+     * @private
+     * @param  {Function} eventTimeout Timeout function
+     * @param  {Object} settings
+     */
+    const eventThrottler = function () {
+        if (!eventTimeout) {
+            eventTimeout = setTimeout(function () {
+                eventTimeout = null;
+                actualMethod(settings);
+            }, 66);
+        }
+    };
+
+    /**
+     * Initialize Plugin
+     * @public
+     * @param {Object} options User settings
+     */
+    myPlugin.init = function (options) {
+
+        // feature test
+        if (!supports) return;
+
+        // Destroy any existing initializations
+        myPlugin.destroy();
+
+        // Merge user options with defaults
+        settings = extend(defaults, options || {});
+
+        // Add class to HTML element to activate conditional CSS
+        document.documentElement.classList.add(settings.initClass);
+
+        // @todo Do something...
+
+        // Listen for events
+        document.addEventListener('click', eventHandler, false);
+
+    };
+
+
+    //
+    // Public APIs
+    //
+
+    return myPlugin;
+
+});
